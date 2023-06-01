73196db39f8d5d4d8e310e0ceec3dc8b9e7aad13
painter98 <emailmepallavi@gmail.com>
2023-06-01 20:53:40 +0530

    change2

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Modal({showModal}){
     return (
-        <div className="model-overlay">
-            <button className='model-close' onClick={()=>showModal(false)}>Close</button>
+        <div className="modal">
+            <button className='modal-close' onClick={()=>showModal(false)}>Close</button>
             <p>This is the content of the modal.</p>
         </div>
     )
